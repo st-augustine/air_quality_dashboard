d2a771344f861d74ae92fdff62ba059b1f0c79da
st-augustine <sophie.porton@towerhamlets.ac.uk>
2023-02-21 10:13:13 +0000

    python dashboard initial commit

---
+++ air_quality_dashbaord.py
@@ -0,0 +1,13 @@
+# %%
+import pandas as pd
+import streamlit as st 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+
+st.set_page_config(layout = "wide")
+st.title("Air quality")
+st.text('This is a web app to allow air quality in Tower Hamlets')
+
+
