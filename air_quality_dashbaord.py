e67af711ed31e9972a7f9bf931576fb946adb4ef
st-augustine <sophie.porton@towerhamlets.ac.uk>
2023-02-22 16:01:33 +0000

    -

--- air_quality_dashbaord.py
+++ air_quality_dashbaord.py
@@ -68,7 +68,7 @@ data = pd.read_sql(sql, conn)
 
 # %%
 #fig, ax =plt.subplots()
-fax=sns.lineplot(x= '@MeasurementDateGMT', y= '@Value', data=data, hue='@Site')
+fig=sns.lineplot(x= '@MeasurementDateGMT', y= '@Value', data=data, hue='@Site')
 st.pyplot(fig)
 
 
